d9e27d4b5e0ce85887ab1077c877554369d6fc31
jalabort <joan.alabort@hudl.com>
2025-06-03 15:52:51 +0100

    feat: Add distortion coeffs to camera parameters container

--- projective_geometry/camera/camera_params.py
+++ projective_geometry/camera/camera_params.py
@@ -5,26 +5,30 @@ from typing import Any
 import numpy as np
 
 from projective_geometry.camera.camera_pose import CameraPose
+from projective_geometry.camera.camera_distortion import CameraDistortion
 
 
 class CameraParams:
-    """Class with camera parameters including camera pose and focal length
+    """Class with camera parameters including camera pose, distortion coefficients and focal length
 
     Attributes:
         camera_pose: contains extrinsic parameters including 3D location/orientation of the camera
+        camera_distorion: contains the distortion coefficients
         focal_length: distance between camera pinhole and film (analog camera)
     """
 
-    def __init__(self, camera_pose: CameraPose, focal_length: float):
+    def __init__(self, camera_pose: CameraPose, camera_distortion: CameraDistortion, focal_length: float):
         self.camera_pose = camera_pose
+        self.camera_distorion = camera_distortion
         self.focal_length = focal_length
+
 
     def to_array(self) -> np.ndarray:
         """Converts to numpy array
         Returns:
-            ndarray  [tx, ty, tz, roll, tilt, pan, focal_length]
+            ndarray  [tx, ty, tz, roll, tilt, pan, k1, k2, p1, p2, k3, focal_length]
         """
-        return np.concatenate((self.camera_pose.to_array(), np.array([self.focal_length])), axis=0)
+        return np.concatenate((self.camera_pose.to_array(), self.camera_distorion.to_array(), np.array([self.focal_length])), axis=0)
 
     def __eq__(self, other: Any, tol: float = 1e-6):
         """Performs the equality comparison between current object and passed one.
@@ -35,8 +39,8 @@ class CameraParams:
             boolean indicating if two objects are equal
         """
         if isinstance(self, other.__class__):
-            return (self.camera_pose == other.camera_pose) and ((self.focal_length - other.focal_length) < tol)
+            return (self.camera_pose == other.camera_pose) and (self.camera_distorion == other.camera_distortion) and ((self.focal_length - other.focal_length) < tol)
         return False
 
     def __repr__(self):
-        return f"CameraParams(camera_pose=CameraPose({str(self.camera_pose)}, focal_length={self.focal_length})"
+        return f"CameraParams(camera_pose=CameraPose({str(self.camera_pose)}, camera_distorion=CameraDistortion({str(self.camera_distorion)}),focal_length={self.focal_length})"
